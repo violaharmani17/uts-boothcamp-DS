630228855da2b0baab00e3893ce1df907f572775
violaharmani17 <73565563+violaharmani17@users.noreply.github.com>
2020-11-27 16:17:45 +0800

    Add files via upload

---
+++ 263 A.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	char a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y;
+	scanf("%c %c %c %c %c\n",a,b,c,d,e);
+	scanf("%c %c %c %c %c\n",f,g,h,i,j);
+	scanf("%c %c %c %c %c\n",k,l,m,n,o);
+	scanf("%c %c %c %c %c\n",p,q,r,s,t);
+	scanf("%c %c %c %c %c\n",u,v,w,x,y);
+	printf("%c %c %c %c %c\n",a,b,c,d,e);
+	printf("%c %c %c %c %c\n",f,g,h,i,j);
+	printf("%c %c %c %c %c\n",k,l,m,n,o);
+	printf("%c %c %c %c %c\n",p,q,r,s,t);
+	printf("%c %c %c %c %c\n",u,v,w,x,y);
+}
